113e9de2ad3a8c95aff3356c02f22e72e76fb979
PhuongHutech <phamphuong11320300@gmail.com>
2022-04-11 00:41:34 +0700

    first commit

---
+++ admincp/modules/footer.php
@@ -0,0 +1,84 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 5%;
+  padding: 10px;
+  height: 100px; /* Should be removed. Only for demonstration */
+
+}
+.d{
+  background-color:#667;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+</style>
+</head>
+<body>
+
+<div class="row">
+  <div class="column" >
+   
+  </div>
+
+<div class="row">
+  <div class="column" >
+   
+  </div>
+<div class="row">
+  <div class="column" >
+   
+  </div>
+<div class="row">
+  <div class="column" >
+   
+  </div>
+<div class="row">
+  <div class="column" >
+   
+  </div>
+<div class="row">
+  <div class="column" >
+   
+  </div>
+  <div class="row">
+  <div class="column" >
+   
+  </div>
+  <div class="row">
+  <div class="column" >
+   
+  </div>
+  <div class="column" >
+     <i class="fab fa-facebook-f" style="font-size:40px;color:lightblue;text-shadow:2px 2px 4px #000000;"></i>
+  </div>
+  <div class="column" >
+    <i class="fab fa-instagram" style="font-size:40px;color:lightblue;text-shadow:2px 2px 4px #000000;"></i>
+  </div>
+  <div class="column" >
+   <i class="fab fa-viber"style="font-size:40px;color:lightblue;text-shadow:2px 2px 4px #000000;"></i>
+  </div>
+   
+   <div class="column" >
+   <i class="fab fa-youtube"style="font-size:40px;color:lightblue;text-shadow:2px 2px 4px #000000;"></i>
+
+  </div>
+</div>
+</body>
+</html>
---
+++ mail/sendmail.php
@@ -0,0 +1,56 @@
+<?php 
+include"PHPMailer/src/PHPMailer.php";
+include"PHPMailer/src/Exception.php";
+include"PHPMailer/src/OAuthTokenProvider.php";
+include"PHPMailer/src/OAuth.php";
+include"PHPMailer/src/POP3.php";
+include"PHPMailer/src/SMTP.php";
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+class Mailer{
+	public function dathangmail($tieude,$noidung,$maildathang){
+	// print_r($mail);
+		$mail = new PHPMailer(true);
+		$mail->CharSet = 'UTF-8';
+		try {
+		    //Server settings
+		    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+		    $mail->isSMTP();                                      // Set mailer to use SMTP
+		    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+		    $mail->Username = 'konynguyen040200@gmail.com';                 // SMTP username
+		    $mail->Password = 'rnnkvhuobsczsfou';                           // SMTP password 
+		    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		    $mail->Port = 587;                                    // TCP port to connect to
+		 
+		    //Recipients
+		    $mail->setFrom('konynguyen040200@gmail.com', 'Mailer');
+		    
+		    $mail->addAddress('kom@gmail.com', 'phamphuong');     // Add a recipient
+		    // $mail->addAddress('','duy');  
+		    // Name is optional
+		    // $mail->addReplyTo('info@example.com', 'Information');
+		    $mail->addCC('konynguyen040200@gmail.com');
+		    // $mail->addBCC('bcc@example.com');
+		 
+		    //Attachments
+		    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		 
+		    //Content
+		    $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->Subject = 'Test mail';
+		    $mail->Body    = 'new content test mail';
+		    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		 
+		    $mail->send();
+		    echo 'Message has been sent';
+
+		} catch (Exception $e) {
+
+		    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+		}
+	}
+}
+?>
